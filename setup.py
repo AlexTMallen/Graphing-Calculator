f9e8d8670acb953e5d4a20986aa5e6254737fd7c
AlexTMallen <35092692+AlexTMallen@users.noreply.github.com>
2018-01-04 10:42:24 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = { "packages": ["os"], "excludes": ["tkinter"], "include_files": ["gc_settings.txt"] }
+
+setup(
+    name = "V3 Graphing Calculator",
+    version = "3.6",
+    description = "Graphing calculator and statistical utility.",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("graphing_calculator.py", base = "Win32GUI")])
